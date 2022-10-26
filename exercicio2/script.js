e96a480f5dcc8328aa46bee8686e4050285e90ee
Anderson-Schi <jr93@hotmai.com>
2022-10-26 22:34:11 +0100

    Exercicio 2 resolvido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,51 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const firstArray = [12, 14, 20, 22, 29, 29];
+const firstArray0 = firstArray[0];
+const firstArray1 = firstArray[1];
+const firstArray2 = firstArray[2];
+const firstArray3 = firstArray[3];
+const firstArray4  = firstArray [4];
+const firstArray5 = firstArray [5];
+const firstVerifi = firstArray.includes(29);
+
+console.log(firstArray0);
+console.log(firstArray1);
+console.log(firstArray2);
+console.log(firstArray3);
+console.log(firstArray4);
+console.log(firstArray5);
+console.log("Quantidade de itens nesse Array:",firstArray.length);
+console.log("O primeiro item desse array:",firstArray0);
+console.log("O item 29 esta inserido nesse Array? ", firstVerifi);
+console.log("-------------------------");
+
+const secondArray = ["Anderson", "Rhaylla", "Anna", "Cristiane", "Luara", "Eduarda" ];
+const secondArray0 = secondArray[0];
+const secondArray1 = secondArray[1];
+const secondArray2 = secondArray[2];
+const secondArray3 = secondArray[3];
+const secondArray4 = secondArray[4];
+const secondArray5 = secondArray[5];
+
+console.log(secondArray0);
+console.log(secondArray1);
+console.log(secondArray2);
+console.log(secondArray3);
+console.log(secondArray4);
+console.log(secondArray5);
+console.log("Quantidade de itens nesse Array:",secondArray.length);
+console.log("O segundo item desse array:",secondArray1);
+console.log("-------------------------");
+
+const thirdArray = ["And", 1993, true ];
+const thirdArray0 = thirdArray[0];
+const thirdArray1 = thirdArray[1];
+const thirdArray2 = thirdArray[2];
+const thirdVerifi = thirdArray.includes(1994);
+
+console.log(thirdArray0);
+console.log(thirdArray1);
+console.log(thirdArray2);
+console.log("Quantidade de itens nesse Array:",thirdArray.length);
+console.log("O terceiro item desse array:",thirdArray2);
+console.log("O item 1994 esta inserido nesse Array? ",thirdVerifi);
+console.log("-------------------------");
